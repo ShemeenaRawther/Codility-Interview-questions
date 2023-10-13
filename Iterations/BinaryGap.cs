e9499018e3d2d446c39b1d54f01f9d5b62b28429
ShemeenaRawther <66967717+ShemeenaRawther@users.noreply.github.com>
2023-10-13 19:13:12 +0530

    added the logic to find the maximum binary gap

---
+++ Iterations/BinaryGap.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Programming
+{
+    internal class BinaryGap
+    {
+        //how to test : call this mthod from Program.cs like eblow
+        //var max= BinaryGap.FindMaxBinaryGap(1021);
+        public static int FindMaxBinaryGap(int n)
+        {
+            if (n == 0 || n < 0)
+            {
+                return 0;
+            }
+            //convert to binary representation
+            string binaryRepresentation = Convert.ToString(n, 2);
+            int maxCountZeros = 0;
+            int countZero = 0;
+            var startOne = false;
+            for (int i = 0; i < binaryRepresentation.Length; i++)
+            {
+                //check if it is binary 1 then increment it
+                if (binaryRepresentation[i] != '1')
+                {
+                    countZero += 1;
+                }
+                //make the start flag true to begin the search of pattern like 1001
+                else if (!startOne)
+                {
+                    startOne = true;
+                }
+                else
+                {
+                    //get the maximum count here
+                    maxCountZeros = Math.Max(maxCountZeros, countZero);
+                    countZero = 0;
+                }
+
+            }
+            return maxCountZeros;
+        }
+    }
+}
