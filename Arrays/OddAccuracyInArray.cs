fdd40a915c0d7fb67efc9483685d9110ad03b2ba
ShemeenaRawther <66967717+ShemeenaRawther@users.noreply.github.com>
2023-10-13 20:02:54 +0530

    Add files via upload

---
+++ Arrays/OddAccuracyInArray.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Programming
+{
+    internal class OddAccuracyInArray
+    {
+        public static int FindOddOccurancy(int[] numbers)
+        {
+            var uniqueNumbers = new HashSet<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (uniqueNumbers.Contains(numbers[i]))
+                {
+                    uniqueNumbers.Remove(numbers[i]);
+                }
+                else
+                {
+                    uniqueNumbers.Add(numbers[i]);
+                }
+            }
+
+            return uniqueNumbers.First();
+
+        }
+    }
+}
